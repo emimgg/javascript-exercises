f3acbfafe0e0404cea8c378066179a6a531f856a
Emiliano G <ultralightened@gmail.com>
2024-06-17 12:57:42 -0400

    Initial commit for sumAll()
    Working for test 1.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    let numberMin = parseInt(min);
+    let numberMax = parseInt(max);
+    let totalSum = 0;
+
+    do {
+        totalSum += numberMin;
+        numberMin++;
+    } while (numberMin <= numberMax);
+
+    console.log(totalSum);
+    return totalSum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
