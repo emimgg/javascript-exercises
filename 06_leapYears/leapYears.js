0a0af1789b8be26a48f21e58413ba5dd72a6d576
Emiliano G <ultralightened@gmail.com>
2024-06-17 14:22:13 -0400

    Initial working commit
    Working for test 1.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    // let isLeapYear = year % 4 === 0 && year % 100 !== 0 ? "is a leap year" : "is not a leap year";
+
+    let isLeapYear = year % 400 === 0 ? true 
+    : year % 4 === 0 ? true : false;
+
+    console.log(isLeapYear);
+    return isLeapYear;
+    
 
 };
 
+leapYears(2000);
 // Do not edit below this line
 module.exports = leapYears;
