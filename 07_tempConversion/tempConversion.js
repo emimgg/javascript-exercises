c42e030742c04f046e62b18f935614763230de06
Emiliano G <ultralightened@gmail.com>
2024-06-17 16:08:57 -0400

    Tweak code to work for tests 1-6.
    Fixed decimal rounding to 1 number.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheit) {
 
-const convertToFahrenheit = function() {
+  let convertedCelsius = ((fahrenheit - 32) * 5/9).toFixed(1);
+  return +convertedCelsius;
+
+}; 
+
+const convertToFahrenheit = function(celsius) {
+
+  let convertedFahrenheit = ((celsius * 9/5) + 32).toFixed(1);
+  return +convertedFahrenheit;
+
 };
 
 // Do not edit below this line
