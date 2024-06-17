28907153fe31671cdcc4020fac5eaca04dd58e84
Emiliano G <ultralightened@gmail.com>
2024-06-17 10:53:53 -0400

    Add working function for test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+
+    const itemsToRemove = Array.from(arguments).slice(1);
+    console.log(itemsToRemove);
+
+    for (let item of itemsToRemove) {
+        let index = array.indexOf(item);
+        let remove = array.splice(index, 1);
+    }
+
+    console.log(array + "array");
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
